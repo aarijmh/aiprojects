bbec534bd204d0ef47f6996689d9e528306cd814
dr.aarij <dr.aarij@DrAarij.GC.Iqra.Local>
2019-04-18 16:14:38 +0500

    pOLICY ITERATION

--- MarkovDecision/com/ai/mdp/element/mdp.py
+++ MarkovDecision/com/ai/mdp/element/mdp.py
@@ -1,4 +1,5 @@
 import sys
+import random
 class MDP(object):
     
     def __init__(self,states,actions,transition,reward,discount=0.5):
@@ -36,3 +37,51 @@ class MDP(object):
                 break
                         
         return previousMatrix, returnQMatrix
+    
+    def policyEvaluation(self,policy,start_v,threshold):
+        while True:
+            delta = 0.0
+            for s in range(len(self._states)):
+                v = start_v[s]
+                actionValue = 0
+                possibleOutcomes = self._transition(s,policy[s])
+                if len(possibleOutcomes) == 0:
+                    actionValue = self._reward(s,policy[s],None)
+                for sp, prob in possibleOutcomes:
+                    actionValue += prob * (self._reward(s,policy[s],sp) + self._discount * start_v[sp])
+                start_v[s] = actionValue
+                delta = max(delta, abs(v - start_v[s]))
+            if delta < threshold:
+                break
+    
+    def policyIteration(self,threshold = 0.000000001):
+        '''intialize the policy and values'''
+        start_v = [random.random() * 100 for _ in self._states]
+        start_policy = [random.randint(0,len(self._actions)-1) for _ in self._states] 
+        
+        
+        while True:
+            policy_stable = True
+            self.policyEvaluation(start_policy, start_v, threshold)
+            
+            for s in range(len(self._states)):
+                old_action = start_policy[s]
+                maxValue = -sys.maxsize - 1                                
+                for a in range(len(self._actions)):
+                    actionValue = 0
+                    possibleOutcomes = self._transition(s,a)                                            
+                    for sp, prob in possibleOutcomes:
+                        actionValue += prob * (self._reward(s,a,sp) + self._discount * start_v[sp])
+                    if maxValue < actionValue:
+                        maxValue = actionValue
+                        start_policy[s] = a
+                if old_action != start_policy[s]:
+                    policy_stable = False
+            if policy_stable:
+                break
+        
+        return start_policy, start_v
+           
+        
+            
+        
--- MarkovDecision/com/ai/mdp/sample/gridmdp.py
+++ MarkovDecision/com/ai/mdp/sample/gridmdp.py
@@ -70,9 +70,9 @@ class GridMDP(object):
         
 
 if __name__ == "__main__":
-    grid = GridMDP("grid.txt",livingReward=-0.4)
+    grid = GridMDP("grid.txt",livingReward=-2.0)
     mdp = MDP(grid._states, grid._actions, grid.transition, grid.reward, .9)
-    v = mdp.valueIteration(100) 
+    v = mdp.policyIteration() 
     print(v[0])
     print(v[1])
 
